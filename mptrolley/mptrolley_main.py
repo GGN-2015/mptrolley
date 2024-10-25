be7048e0ad433fb543e6eff6fb52108cbf6619b1
GGN_2015 <premierbob@qq.com>
2024-10-25 21:34:20 +0800

    v0.0.3

--- mptrolley/mptrolley_main.py
+++ mptrolley/mptrolley_main.py
@@ -1,25 +1,6 @@
-import multiprocessing
+import process_wrap_queue
+from rich.progress import Progress, TextColumn, BarColumn, TimeElapsedColumn, TimeRemainingColumn
 import time
-import sys
-
-# timer and verbose output
-BEGIN_TIME = time.time()
-def verbose_output(verbose:bool, msg: str): # output verbose info to stderr
-    if verbose:
-        timenow = time.time()
-        sys.stderr.write("[%13.3f] %s\n" % (timenow-BEGIN_TIME, msg.strip()))
-
-# to create a single process
-# return the process object just after it is created
-def create_process_for_problem(verbose:bool, question_function, common_context:dict, question_count:int, process_count:int, process_id:int) -> multiprocessing.Process:
-    def worker_process():
-        for question_id in range(1, question_count):      # question 0 should be skipped
-            if question_id % process_count == process_id: # select some question to solve
-                verbose_output(verbose, "START: process_id: %4d, question_id: %8d/%8d" % (process_id, question_id, question_count))
-                question_function(question_id, common_context)
-                verbose_output(verbose, "DONE : process_id: %4d, question_id: %8d/%8d" % (process_id, question_id, question_count))
-    process_now = multiprocessing.Process(target=worker_process, args=())
-    return process_now
 
 """
 question_function(question_id: int, common_context: dict):
@@ -43,26 +24,52 @@ problem:
 attention:
     the user needs to ensure that the function question_function can be called in parallel.
 """
-def solve_problem_with_multiprocessing(question_function, common_context: dict, question_count: int, process_count: int, verbose=True):
+def solve_problem_with_multiprocessing(question_function, common_context: dict, question_count: int, process_count: int):
     assert question_count >= 1
     assert process_count > 0
 
-    # test phase: solve the first question (without parallel)
-    # to prevent some basic errors which may occured in the sub process.
-    verbose_output(verbose, "START: process_id: main, question_id: %8d/%8d" % (0, question_count))
-    question_function(0, common_context)
-    verbose_output(verbose, "DONE : process_id: main, question_id: %8d/%8d" % (0, question_count))
+    # create pw wrap queue
+    pw_queue_list = []
+    for _ in range(process_count):
+        pw_queue = process_wrap_queue.ProcessWrapQueue()
+        pw_queue_list.append(pw_queue)
 
-    # solve phase: create all process
-    process_pool = []
-    for process_id in range(process_count):
-        process_now = create_process_for_problem(verbose, question_function, common_context, question_count, process_count, process_id)
-        process_now.start()
-        process_pool.append(process_now)
+    # create all inner_process_wrap in subprocess
+    for i in range(question_count):
+        pw            = process_wrap_queue.InnerProcessWrap(question_function, args=(i, common_context))
+        process_index = i % process_count
+        pw_queue_list[process_index].add_process_wrap(pw) # add inner process wrap
 
-    # waiting phase: wait all process to finish
-    for process_obj in process_pool:
-        process_obj.join()
+    def get_total_job_count(pw_queue) -> int:
+        brief     = pw_queue.get_queue_status_brief()
+        total_cnt = brief["term_queue_len"] + brief["run_queue_len"] + brief["pend_queue_len"]
+        return total_cnt
+
+    def get_finished_job_count(pw_queue) -> int:
+        return pw_queue.get_queue_status_brief()["term_queue_len"]
+    
+    # show process progress bar
+    task_list = []
+    with Progress(
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),
+        TextColumn("[progress.percentage]{task.completed}/{task.total}"),
+        TimeElapsedColumn(),
+        TimeRemainingColumn()
+    ) as progress:
+        for idx, pw_queue in enumerate(pw_queue_list):
+            task = progress.add_task("process %3d: " % idx, total=get_total_job_count(pw_queue))
+            task_list.append(task)
+
+        # set all status to AUTO
+        for pw_queue in pw_queue_list:
+            pw_queue.set_queue_status("AUTO") # auto quit when finished
+
+        # wait
+        while not progress.finished:
+            time.sleep(0.2)
+            for i in range(len(task_list)):
+                progress.update(task_list[i], completed=get_finished_job_count(pw_queue_list[i]))
 
 
 
@@ -80,7 +87,7 @@ def run_sample_problem(question_cnt, process_cnt):
 
     def sample_question_function(qid: int, ctx: dict): # create may file in a folder
         os.makedirs(test_case, exist_ok=True)
-        data_file  = os.path.join(test_case, "%5d.txt" % qid)
+        data_file  = os.path.join(test_case, "%05d.txt" % qid)
         raw_string = "%d" % qid
         for _ in range(1000000): # calculate sha256sum for many times (to waste time)
             raw_string = calculate_sha256(raw_string)
@@ -88,4 +95,4 @@ def run_sample_problem(question_cnt, process_cnt):
     solve_problem_with_multiprocessing(sample_question_function, {}, question_cnt, process_cnt)
 
 if __name__ == "__main__": # this is just a testcase
-    run_sample_problem(100, 32)
+    run_sample_problem(100, 5)
